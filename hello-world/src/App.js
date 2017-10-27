b546a73aa00e0b336fc4d5a89645962abc23208b
alexandraalday <alexandra.alday@gmail.com>
2017-10-26 21:06:41 -0700

    using jsx

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-class App extends Component {
+class Hello extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Hello World</h1>
+        <h3>It is time for tea</h3>
       </div>
-    );
+      )
   }
 }
 
-export default App;
+export default Hello
