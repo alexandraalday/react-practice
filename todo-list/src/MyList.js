09e67d2b2e9843d43aef6403340879a9b720f12e
alexandraalday <alexandra.alday@gmail.com>
2017-10-27 21:37:23 -0700

    can add a new item

--- todo-list/src/MyList.js
+++ todo-list/src/MyList.js
@@ -6,16 +6,32 @@ class MyList extends Component {
   constructor (props) {
     super()
     this.state = {
-      toDoItemArray: props.theList
+      toDoItemArray: props.theList,
+      newItem: ''
     }
   }
 
   clearList (e) {
-  console.log("Awww yiss cleared the list!") // sanity check
-  this.setState({
-    toDoItemArray: []
-  })
-}
+    console.log("Awww yiss cleared the list!") // sanity check
+    this.setState({
+      toDoItemArray: []
+    })
+  }
+
+  newItemChange(e) {
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  addItem(e) {
+    // prevent the event from running the default "submit" event.
+    e.preventDefault()
+    console.log("Added a new item yo!") // sanity check
+    let toDos = this.state.toDoItemArray
+    toDos.push(this.state.newItem)
+    this.setState({toDoItemArray: toDos, newItem: ''})
+  }
 
   render() {
     let todoItems = this.state.toDoItemArray.map((item, index) => (
@@ -24,11 +40,19 @@ class MyList extends Component {
 
     return (
       <div>
-        <h1>Things I should maybe not procrastinate:</h1>
-          <ul>
-            {todoItems}
-          </ul>
-          <button onClick={(e) => this.clearList(e)}>Finished the list!</button>
+        <h1>Things I should stop procrastinating:</h1>
+        <ul>
+          {todoItems}
+        </ul>
+        <form>
+          <input type="text"
+           placeholder="Type an item here"
+           onChange={(e) => this.newItemChange(e)}
+           value={this.state.newItem}
+          />
+          <button onClick={(e) => this.addItem(e)}>Add it!</button>
+        </form>
+        <button onClick={(e) => this.clearList(e)}>Finished the list!</button>
       </div>
       
     );
