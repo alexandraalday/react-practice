c7f86b525e3e88b64fec02c12cdc1e5a18ba39e3
alexandraalday <alexandra.alday@gmail.com>
2017-10-27 22:58:15 -0700

    adds ability to multiply values

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -8,6 +8,7 @@ class Calculator extends Component {
     // This binding is necessary to make `this` work in the callback
     this.add = this.add.bind(this)
     this.subtract = this.subtract.bind(this)
+    this.multiply = this.multiply.bind(this)
 
     this.state = {
       result: 0
@@ -30,6 +31,14 @@ class Calculator extends Component {
     })
   }
 
+  multiply() {
+    let product = (parseInt(this.refs.val1.value) || 0) * (parseInt(this.refs.val2.value) || 0);
+
+    this.setState({
+      result: product
+    })
+  }
+
   render() {
     return (
 		<div className="container">
@@ -39,6 +48,8 @@ class Calculator extends Component {
 			<input type="text" ref="val1" />
           	<button onClick={this.add}>+</button>
 			<button onClick={this.subtract}>-</button>
+          	<button onClick={this.multiply}>*</button>
+
           	<input type="text" ref="val2" />
           	<span>=</span>
 			<h3>{this.state.result}</h3>
