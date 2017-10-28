a75256b0ae1c3346874bf64be2e574442ecbb693
alexandraalday <alexandra.alday@gmail.com>
2017-10-27 22:54:08 -0700

    adds ability to subtract values

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -7,31 +7,41 @@ class Calculator extends Component {
     
     // This binding is necessary to make `this` work in the callback
     this.add = this.add.bind(this)
+    this.subtract = this.subtract.bind(this)
 
     this.state = {
-      sum: 0
+      result: 0
 	}
   }
 
   add() {
-    var sum = (parseInt(this.refs.val1.value) || 0) + (parseInt(this.refs.val2.value) || 0);
+    let sum = (parseInt(this.refs.val1.value) || 0) + (parseInt(this.refs.val2.value) || 0);
 
     this.setState({
-      sum: sum
+      result: sum
+    })
+  }
+
+  subtract() {
+    let diff = (parseInt(this.refs.val1.value) || 0) - (parseInt(this.refs.val2.value) || 0);
+
+    this.setState({
+      result: diff
     })
   }
 
   render() {
     return (
 		<div className="container">
-		  <h1>Add with React!</h1>
+		  <h1>Math with React!</h1>
 
 		  <div className="add">
-			<input type="text" ref="val1" onKeyUp={this.add} />
-          	<span>+</span>
-          	<input type="text" ref="val2" onKeyUp={this.add} />
+			<input type="text" ref="val1" />
+          	<button onClick={this.add}>+</button>
+			<button onClick={this.subtract}>-</button>
+          	<input type="text" ref="val2" />
           	<span>=</span>
-			<h3>{this.state.sum}</h3>
+			<h3>{this.state.result}</h3>
 		  </div>
 		</div>
     );
