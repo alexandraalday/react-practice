6edd44b0345f376024ae187a29d73330ebfbf972
alexandraalday <alexandra.alday@gmail.com>
2017-10-27 20:08:12 -0700

    nested objects and some styles

---
+++ todo-list/src/MyList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import ListItem from './ListItem'
+
+class MyList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Things I should maybe not procrastinate:</h1>
+          <ul>
+            <ListItem doThis="Buy ice cream" />
+          </ul>
+      </div>
+      
+    );
+  }
+}
+
+export default MyList;
