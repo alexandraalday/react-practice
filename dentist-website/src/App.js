5348ba603e1ea33659c12c7020e5a6721ee8087b
alexandraalday <alexandra.alday@gmail.com>
2017-10-28 20:57:14 -0700

    adds nav bar

--- dentist-website/src/App.js
+++ dentist-website/src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 import Home from './Home'
 import Procedures from './Procedures'
@@ -7,11 +12,18 @@ import Contact from './Contact'
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home />
-        <Procedures />
-        <Contact />
-      </div>
+      <Router>
+        <div>
+        <nav>
+          <Link to="/">Home</Link>{' '}
+          <Link to="/procedures">Procedures</Link>{' '}
+          <Link to="/contact">Contact</Link>
+        </nav>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/procedures" component={Procedures} />
+          <Route exact path="/contact" component={Contact} />
+        </div>
+      </Router>
     );
   }
 }
